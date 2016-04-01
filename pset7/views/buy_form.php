2d5370aded5f7dccdf5d5b1e1f4e16becdaf175c
AJ BC <andresmymail@gmail.com>
2016-04-01 04:56:27 +0000

    Background color change, and extra image added

--- pset7/views/buy_form.php
+++ pset7/views/buy_form.php
@@ -1,6 +1,6 @@
 <form action="buy.php" method="post"> 
 <fieldset>
-
+<body style="background-color: #FFFFF0">
     <div class="form-group">
                 <input class="form-control" name="symbol" placeholder="Symbol" type="code"/>
     </div>
@@ -15,6 +15,6 @@
             Submit
         </button>
     </div>
-
+<img src= "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSe68eid59ynXv-kiliCeEYOF5KslDHOdsrm7B5pEmeKBCBWYF4" style = "width: 700x; height: 200px"/>
 </fieldset>
 </form>
