8ec6e7b33dc4b876374a257652e91f3c070d9b40
AJ BC <andresmymail@gmail.com>
2016-04-01 05:14:53 +0000

    Background color change and extra photo

--- pset7/views/quote_views.php
+++ pset7/views/quote_views.php
@@ -1,3 +1,5 @@
+<body style="background-color: #F5F5F5">   
+
 <h3><?= $stock["symbol"] ?></h3>
 <h2><?= $stock["name"] ?></h2>
 Price: $<?= $stock["price"] ?>
@@ -14,3 +16,5 @@ Price: $<?= $stock["price"] ?>
     <button onClick="window.location='buydirect.php';" <button class="btn btn-default" type="submit">
         <span aria-hidden="true" class="glyphicon glyphicon-log-in"></span> Buy This Stock</button>
 </div>
+
+<img src= "http://www.technobuffalo.com/wp-content/uploads/2011/12/ul_BuyItNow_green_story.jpg" style = "width: 700x; height: 200px"/>
